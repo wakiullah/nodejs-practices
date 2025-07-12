f66144f37768b7ddf6f1e119aa8d51bff3f19e3e
wakiullah <m.wakiullah1212@gmail.com>
2025-07-12 15:03:27 +0600

    practice more things about response

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ const app = express();
 app.use(express.json())
 
 const backend = express()
-
-backend.get('/dahsboard', (req, res) => {
+app.set('view engine', 'ejs')
+backend.get('/dashboard', (req, res) => { // fixed typo
     res.send("this is backend dashboard")
 })
 
 app.post('/hello/:id', (req, res) => {
     console.log(req.params.id);
-
-    res.send('dthis is get')
-
+    res.send('this is post') // fixed response message
 })
 app.use('/backend', backend)
 
+app.get('/about', (req, res) => {
+    res.render('pages/about') // fixed render path
+})
 
 app.listen(3000, () => {
     console.log('app is listening on port 3000');
-
 })

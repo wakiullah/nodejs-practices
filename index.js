37a0ba41c258abbaef4cba121a091452f4c4b0c1
wakiullah <m.wakiullah1212@gmail.com>
2025-07-14 22:07:02 +0600

    express rouer method

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
 const express = require('express')
 
 const app = express();
-app.use(express.json())
-
-const backend = express()
-app.set('view engine', 'ejs')
-backend.get('/dashboard', (req, res) => { // fixed typo
-    res.send("this is backend dashboard")
-})
-
-app.post('/hello/:id', (req, res) => {
-    console.log(req.params.id);
-    res.send('this is post') // fixed response message
-})
-app.use('/backend', backend)
-
 app.get('/about', (req, res) => {
-    res.render('pages/about') // fixed render path
+    const oab = new URL(req.url, `http://${req.headers.host}`);
+
+    const detail = Object.fromEntries(oab.searchParams)
+    console.log(oab.searchParams);
+
+
+    res.send('about page')
 })
 
 app.listen(3000, () => {
     console.log('app is listening on port 3000');
 })
 
-const myUrl = new URL('https://www.example.com:8080/path/to/resource?name=John&age=30#section-id');
-console.log(myUrl);
+// const myUrl = new URL('https://www.example.com:8080/path/to/resource?name=John&age=30#section-id');
+// console.log(myUrl);

00c2ce9b703a0f849227cdc5952944f304fc8885
wakiullah <m.wakiullah1212@gmail.com>
2025-07-21 16:12:59 +0600

    mongodb schema

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import e from "express";
 import mongoose from "mongoose";
+const todoHandler = require('./routeHandler/TodoHandler')
 const app = e()
 
+mongoose.connect('mongodb://localhost/todos', {
 
-
-app.get('/', (req, res) => {
-    res.send('hello')
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
+    .then(() => console.log('connected')
+    )
+    .catch((err) => console.log(err)
+    )
+
+app.get('/', todoHandler)
 
 app.listen(3000, () => {
     console.log('app is running');

b5b9f8b09cf50e0276a159521c94b09c1c1817a7
Md Wakiullah <74135682+wakiullah@users.noreply.github.com>
2025-08-09 00:46:12 +0600

    Update loginRouter.js

--- router/loginRouter.js
+++ router/loginRouter.js
@@ -7,7 +7,6 @@ const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse
 
 const router = express.Router();
 
-// login page
 router.get("/", decorateHtmlResponse("Login"), getLogin);
 
 module.exports = router;

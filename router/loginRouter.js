9d2667e76730f42b8512bf25baeeeb781e2dfe72
Md Wakiullah <74135682+wakiullah@users.noreply.github.com>
2025-08-09 00:46:27 +0600

    Update loginRouter.js

--- router/loginRouter.js
+++ router/loginRouter.js
@@ -4,9 +4,9 @@ const express = require("express");
 // internal imports
 const { getLogin } = require("../controller/loginController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
-
 const router = express.Router();
 
 router.get("/", decorateHtmlResponse("Login"), getLogin);
+
 
 module.exports = router;
